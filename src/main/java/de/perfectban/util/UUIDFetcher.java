c4aeb7e187931959ef2adf0a11419c62e1896db1
Christian Tschörner <christian@tschoerner.org>
2021-11-15 21:28:53 +0100

    [#PB-7] UUIDFetcher - Added UUIDFetcher.java

---
+++ src/main/java/de/perfectban/util/UUIDFetcher.java
@@ -0,0 +1,47 @@
+package de.perfectban.util;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+
+/**
+ * @author Christian Tschörner
+ */
+public class UUIDFetcher {
+
+    private static final HashMap<String, UUID> CACHE = new HashMap<>();
+
+    private static final String UUID_URL = "https://minecraft-api.com/api/uuid/%s";
+
+    public static void getUUIDbyName(String name, Consumer<UUID> callback){
+        if(CACHE.containsKey(name.toUpperCase())){
+            callback.accept(CACHE.get(name.toUpperCase()));
+            return;
+        }
+
+        Executors.newCachedThreadPool().execute(() -> {
+            try {
+                String trimmedUUID = new HttpGetRequest(String.format(UUID_URL, name))
+                        .send()
+                        .getAsString();
+
+                UUID uuid = fromTrimmed(trimmedUUID);
+
+                CACHE.put(name.toUpperCase(), uuid);
+                callback.accept(uuid);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+    }
+
+    private static UUID fromTrimmed(String uuid) {
+        Pattern pattern = Pattern.compile("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})");
+        return UUID.fromString(pattern.matcher(uuid.replace("-", ""))
+                .replaceAll("$1-$2-$3-$4-$5"));
+    }
+}
